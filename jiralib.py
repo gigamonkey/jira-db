b026b4eb118a4b1eec70230d2414d2e6645ce17e
Peter Seibel <seibelp@dnc.org>
2019-07-10 15:29:10 -0700

    Cleaning up extractors.

--- jiralib.py
+++ jiralib.py
@@ -5,36 +5,10 @@ from re import compile
 import arrow
 
 #
-# Because the Jira API is awesome, the values in the list of sprints a
-# Task has been part of (a.k.a. customfield_10008) are rendered--in
-# the middle of a JSON response--as strings that appears to have been
-# produced by calling toString() on some Java object. So we tear that
-# apart with some regexps to get at the actual values. So elegant!
-#
-
-sprint_re = compile(
-    r"^com\.atlassian\.greenhopper\.service\.sprint\.Sprint@.*?\[(.*?)\]"
-)
-
-sprint_values_re = compile(r"(\w+)=(.*?)(?:,|$)")
-
-
-def sprints(issue):
-    "Extract the name of the sprints a Task has been added to."
-
-    def g():
-        for desc in issue.get("fields", {}).get("customfield_10008") or []:
-            m = sprint_re.match(desc)
-            if m:
-                yield dict(sprint_values_re.findall(m.group(1)))
-
-    return list(g())
-
-
-#
 # Map the field names we want to use to the actual terrible names Jira
 # uses. (True means the name is the same in Jira.)
 #
+
 jira_fields = {
     "assignee": True,
     "component": "components",
@@ -54,23 +28,66 @@ jira_fields = {
 #
 # Functions to extract the field value from an issue.
 #
+
+
+def field(*names, fn=None):
+    fn = fn or (lambda x: x)
+
+    def extract(issue):
+        value = issue["fields"]
+        for n in names:
+            value = value.get(n)
+            if value is None:
+                break
+        return fn(value)
+
+    return extract
+
+
+def timestamp(s):
+    "Convert a timestame string to ISO in UTC."
+    return arrow.get(s).to("utc").isoformat() if s else None
+
+
+#
+# Because the Jira API is awesome, the values in the list of sprints a
+# Task has been part of (a.k.a. customfield_10008) are rendered--in
+# the middle of a JSON response--as strings that appears to have been
+# produced by calling toString() on some Java object. So we tear that
+# apart with some regexps to get at the actual values. So elegant!
+#
+
+sprint_re = compile(
+    r"^com\.atlassian\.greenhopper\.service\.sprint\.Sprint@.*?\[(.*?)\]"
+)
+
+sprint_values_re = compile(r"(\w+)=(.*?)(?:,|$)")
+
+
+def parse_sprint(s):
+    m = parse_sprint.sprint_re.match(s)
+    return dict(sprint_values_re.findall(m.group(1))) if m else None
+
+
+def extract_sprints(value):
+    return [s for s in (parse_sprint(s) for s in value) if s] if value else []
+
+
 extractors = {
-    "assignee": lambda x: (x["fields"]["assignee"] or {"displayName": None})[
-        "displayName"
-    ],
-    "components": lambda x: [c["name"] for c in x["fields"]["components"]],
-    "created": lambda x: timestamp(x["fields"]["created"]),
-    "epic": lambda x: x["fields"].get("customfield_10006"),
-    "epic_name": lambda x: x["fields"].get("customfield_10003"),
-    "issue_type": lambda x: x["fields"]["issuetype"]["name"],
+    "assignee": field("assignee", "displayName"),
+    "components": field("components", fn=lambda cs: [c["name"] for c in cs]),
+    "created": field("created", fn=timestamp),
+    "epic": field("customfield_10006"),
+    "epic_name": field("customfield_10003"),
+    "issue_type": field("issuetype", "name"),
     "key": lambda x: x["key"],
-    "parent": lambda x: x["fields"]["parent"]["key"],
-    "resolution": lambda x: (x["fields"].get("resolution") or {"name": None})["name"],
-    "resolved": lambda x: timestamp(x["fields"]["resolutiondate"]),
-    "sprints": sprints,
-    "status": lambda x: x["fields"]["status"]["name"],
-    "summary": lambda x: x["fields"]["summary"],
-    "updated": lambda x: timestamp(x["fields"]["updated"]),
+    "parent": field("parent", "key"),
+    "resolution": field("resolution", "name"),
+    "resolved": field("resolutiondate", fn=timestamp),
+    "sprints": field("customfield_10008", fn=extract_sprints),
+    "status": field("status", "name"),
+    "summary": field("summary"),
+    "updated": field("updated", fn=timestamp),
 }
 
 
@@ -105,11 +122,6 @@ def translate_fields(fields):
 
 def extract(field, issue):
     return extractors[field](issue)
-
-
-def timestamp(s):
-    "Convert a timestame string to ISO in UTC."
-    return arrow.get(s).to("utc").isoformat() if s else None
 
 
 def changes(issue):
