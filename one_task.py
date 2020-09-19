df1617c79df7bf0c3eb62e3dcf51acc33c2e0636
Peter Seibel <seibelp@dnc.org>
2020-09-19 11:09:01 -0700

    Updates to onetask script.

--- one_task.py
+++ one_task.py
@@ -6,7 +6,23 @@ import sys
 
 from jira import JIRA
 
-from jiralib import issues
+
+def issues(client, jql, fields=[]):
+    "Fetch issues from Jira"
+    start = 0
+    while start is not None:
+        r = client.search_issues(
+            jql,
+            startAt=start,
+            maxResults=50,
+            json_result=True,
+            # fields=fields,
+            # expand="changelog",
+        )
+        batch = r["issues"]
+        start = start + len(batch) if start < r["total"] else None
+        yield from batch
+
 
 if __name__ == "__main__":
 
@@ -15,5 +31,24 @@ if __name__ == "__main__":
     key = os.environ["JIRA_KEY"]
     client = JIRA(url, basic_auth=(account, key))
 
-    for issue in issues(client, f"key = {sys.argv[1]}"):
+    fields = [
+        "assignee",
+        "changelog",
+        "components",
+        "created",
+        "customfield_10003",
+        "customfield_10006",
+        "customfield_10008",
+        "duedate",
+        "issuetype",
+        "parent",
+        "project",
+        "resolution",
+        "resolutiondate",
+        "status",
+        "summary",
+        "updated",
+    ]
+
+    for issue in issues(client, f"key = {sys.argv[1]}", fields=fields):
         json.dump(issue, fp=sys.stdout, indent=2)
